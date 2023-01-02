932700867ed7b6d6ac44ed271adb72fa65113ca7
ramazan <balaban1432@gmail.com>
2023-01-02 23:54:02 +0300

    02.01.2023

--- BTK-course/ders_4.py
+++ BTK-course/ders_4.py
@@ -1,3 +1,4 @@
+# Tupples
 # tuple1 = 1, "iki", 3  # parantez koymasak da tupple oluyor
 # tuple2 = (1, "iki", 3)
 
@@ -13,6 +14,17 @@
 
 # tuple2 = (45, 69) + mytupple
 # print(tuple2)
+
+# Dictioneries
+
+# plakalar = {"kocaeli" : 41, "istanbul" : 34}
+# print(plakalar["kocaeli"])
+# print(plakalar["istanbul"])
+
+# plakalar["ankara"] = 6
+# print(plakalar)
+# plakalar["istanbul"] = "nev value"
+# print(plakalar)
 
 
 

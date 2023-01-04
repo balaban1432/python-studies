9874c14efab7c2a81d54a036c5a0bfa26f4caf40
ramazan <balaban1432@gmail.com>
2023-01-04 12:14:57 +0300

    son

---
+++ BTK-course/ders_5.py
@@ -0,0 +1,34 @@
+# atama operatörleri
+# x, y, z = 5, 16, 20
+
+# x += 5
+# x -= 5
+# x *= 5
+# x /=5
+# x %= 5
+# y //= 5
+# x **= 5
+# y *= z 
+
+# print(x, y, z)
+
+
+# values = 1, 2, 3
+
+# print(values)
+# print(type(values))
+
+# x, y, z = values  # unpacking
+
+# print(x, y, z)  # unpack yapabilmek için eksik veya fazla öğe olmamalı
+
+# eğer fazla öğe varsa şu yapılabilir.
+# values = 1, 2, 3, 4, 5
+
+# x, y, *z = values  # kalanları z ye liste olarak atar
+
+# print(x, y, z, sep="\n")
+
+
+
+

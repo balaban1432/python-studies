8868ef6210dbe5bfb786787bd006c01478667d06
ramazan <balaban1432@gmail.com>
2023-01-07 23:56:23 +0300

    son

--- BTK-course/ders_5.py
+++ BTK-course/ders_5.py
@@ -30,5 +30,45 @@
 # print(x, y, z, sep="\n")
 
 
+# x, y, z = 2, 5, 10
+
+# numbers = 1, 5, 7, 10, 6
+
+# 1. what is the subtraction of sum  x,y,z to multiplation of the two number that given from user
+# num1 = int(input("enter first number"))
+# num2 = int(input("enter second number"))
+
+# print((num1*num2) - (x + y + z))
+
+# 2. y // x = ?
+# print(y // x)
+
+# 3. what is modulus of sum of x,y,z?
+
+# print((x + y + z) % 3)
+
+# 4. y  ** x
+
+# print(y ** x ) 
+
+# 5. 
+
+# x , z, *y = numbers
+# print(x , y, z)
+# print(z**3)
+# print(sum(y))
+
+
+# karşılaştırma operatörleri
+
+# a, b, c, d = 5, 5, 10, 4
+
+# result = (a==b)
+# result = (a==c)
+
+# print(result)
+
+
+# print(False==0)
 
 

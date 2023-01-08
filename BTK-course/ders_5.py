1a0053f342f3565986e95e2e7afd313435528f1a
ramazan <balaban1432@gmail.com>
2023-01-08 08:53:46 +0300

    08.01.2023

--- BTK-course/ders_5.py
+++ BTK-course/ders_5.py
@@ -72,3 +72,78 @@
 # print(False==0)
 
 
+# vize1 = int(input("enter first vise"))
+# vize2 = int(input("enter second vise"))
+# final = int(input("enter final result"))
+
+# average = ((vize1 + vize2) / 2) * 0.4 + final * 0.6
+# print(f"your mark is: {average}")
+
+
+# manyıksal operatörler
+
+# x= 5
+# name = "ramazan"
+# result = 5 < x < 10
+
+# print(result)
+
+# and
+# result = (x > 5) and (x < 10)
+# print(result)
+
+# result = (x==5) and (name == "ramazan") 
+# print(result)
+
+
+# or
+# result = (x > 0) or (x % 2 == 0)
+# print(result)
+
+# not
+
+# result = not True
+# print(result)
+
+
+# number = int(input("enter a number"))
+
+# result = (number > 0) and (number % 2 == 0)
+# print(f"number is a positive even number : {result} ")
+
+
+# other python operators
+
+# Identity operator : is
+
+# x = y = [1, 2, 3]
+# z = [1, 2, 3]
+
+# print(x == y)
+# print(x == z)
+# print(x is y)
+# print(x is z) # is operatöründe değerlerin aynı olması önemli değil. objelerin aynı olmasına bakılır.
+
+
+# x = [1, 2, 3]
+# y = [2, 4]
+
+# del x[2]
+# y[1] = 1
+# y.reverse()
+
+# print(x == y)
+# print(x is y)
+# print(x is not y)
+
+
+# membership operator : in
+
+# x = ["apple", "banana"]
+# print("banana" in x)
+# print("apple" not in x)
+
+
+
+
+

623a8f2bdd2a58bf5c693f588595c9e532b15c88
ramazan <balaban1432@gmail.com>
2023-01-12 01:12:23 +0300

    12.01.2023

--- BTK-course/ders_6.py
+++ BTK-course/ders_6.py
@@ -84,10 +84,73 @@
 # elif days > 365*2 and days <= 365*3:
 #     print("3. servis aralığı")
 # else:
-#     print("hatalı süre!")    
+#     print("hatalı süre!") 
 
 
+# 4. 
+# number = int(input("enter a number: "))
+
+# if number > 0 and number < 100:
+#     print("your number is between 0 to 100")
+# elif number >= 100:
+#     print("your number is greater than 100")
+# else:
+#     print("your number is smaller than 0")    
 
 
+# 5. check the number if it is positive even number. 
+# number = int(input("enter a number: "))
 
+# if number > 0:
+#     if number % 2 == 0:
+#         print(f"{number} is a positive even number")
+#     else:
+#         print(f"{number} is a positive odd number")   
+# else:
+#     print(f"{number} is a negative number")     
+
+
+# 6. compare given three numbers
+# number1 = int(input("enter first number: "))
+# number2 = int(input("enter second number: "))
+# number3 = int(input("enter third number: "))
+
+# if number1 > number2 and number1 > number3:
+#     print(f"{number1} is the biggest number")
+# elif number2 > number1 and number2 > number3:
+#     print(f"{number2} is the biggest number")
+# else:
+#     print(f"{number3} is the biggest number")
+
+
+# 7.
+# vise1 = int(input("enter first vise result: "))
+# vise2 = int(input("enter second vise result: "))
+# final = int(input("enter final result: "))
+
+# average = (vise1 + vise2) * 0.40 + final * 0.60
+
+# if final >= 70:
+#     print("you have passed.")
+# elif average >= 50 and final >= 50:
+#     print("you have passed.")
+# else:
+#     print("you have not passed.")
+
+
+# 8.
+# name = input("enter your name: ")
+# weight = float(input("enter your weight: "))
+# length = float(input("enter your length: "))
+
+# kilo_index = weight / (length ** 2)
+
+# if kilo_index >= 0 and kilo_index <= 18.4:
+#     print(f"{name} is thin")
+# elif kilo_index >= 18.5 and kilo_index <= 24.9:
+#     print(f"{name} is normal")
+# elif kilo_index >= 25.0 and kilo_index <= 29.9:
+#     print(f"{name} is fat")
+# else:
+#     print(f"{name} is obese")
 

2015b20edbb0a1b7baf7903c569448b439885a55
ramazan <balaban1432@gmail.com>
2023-01-13 16:01:51 +0300

    ss

---
+++ teamwork/10_team_work.py
@@ -0,0 +1,17 @@
+
+def my_function(s):
+    stack = []
+    lookup = {")" : "(", "}" : "{", "]" : "["}
+
+    for p in s:
+        if p in lookup.values():
+            stack.append(p)
+        elif stack and lookup[p] == stack[-1]:
+            stack.pop()
+        else:
+            return False
+
+    return stack == [] 
+
+s = "[]{}"
+print(my_function(s))
--- test.py
+++
@@ -1,2 +0,0 @@
-x = sum(range(101))
-print(x)

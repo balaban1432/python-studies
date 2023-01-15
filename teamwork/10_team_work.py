8c67dc8c2cc9e54c36ad549fba7aff06c12d5f44
ramazan <balaban1432@gmail.com>
2023-01-15 08:30:41 +0300

    15.01.2023

--- teamwork/10_team_work.py
+++ teamwork/10_team_work.py
@@ -1,30 +1,6 @@
 
-def my_function(s):
-    stack = []
-    lookup = {")" : "(", "}" : "{", "]" : "["}
-
-    for p in s:
-        if p in lookup.values():
-            stack.append(p)
-        elif stack and lookup[p] == stack[-1]:
-            stack.pop()
-        else:
-            return False
-
-    return stack == [] 
-
-s = "[]{}"
-print(my_function(s))
-
-
-
-
-
-
-
 combination = input('Write a string that contains only `(`, `)`, `{`, `}`, `[` and `]`: ')
 
-#([])
 def isValid(par):
     bracket_map = {
         "(": ")",
@@ -32,15 +8,15 @@ def isValid(par):
         "{": "}"
     }
     openP = {"(", "[", "{"}
-    stack = [] #(
-    for el in par: # )
+    stack = [] 
+    for el in par: 
         if el in openP:
             stack.append(el)
-        elif el == bracket_map[stack[-1]]: #stakc son elaman ( -> )
+        elif el == bracket_map[stack[-1]]: 
             stack.pop()
         else:
             return False
-    return stack == [] #boolean
+    return stack == [] 
 
 
 print(isValid(combination))

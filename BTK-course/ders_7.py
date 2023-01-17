18fd73bbe33b7a68bd0bd34fd5c8adb75ccb9b43
ramazan <balaban1432@gmail.com>
2023-01-17 19:32:19 +0300

    dd

---
+++ BTK-course/ders_7.py
@@ -0,0 +1,25 @@
+# numbers = [1, 2, 3, 4, 5]
+
+# for num in numbers:
+#     print(num)
+
+
+# names = ["ramazan", "derya", "kemal"]
+
+# for name in names:
+#     print(f"my name is {name}")
+
+
+# name = "ramazan balaban"
+
+# for i in name:
+#     print(i)
+
+
+# tuple = 1, 2, 3, 4, 5
+
+# for i in tuple:
+#     print(i)
+
+
+

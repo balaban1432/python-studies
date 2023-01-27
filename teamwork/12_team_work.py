9fb92da1eb847b13583599d827c460df4d5a3bf3
ramazan <balaban1432@gmail.com>
2023-01-27 11:54:20 +0300

    27.01.2023

--- teamwork/12_team_work.py
+++ teamwork/12_team_work.py
@@ -9,12 +9,12 @@ import random
 result = ""
 k = 1
 
-while k < 5:
+while k < 8:
     if k < 4:
         i = random.randint(0, (len(list_name) - 1))
         result += list_name[i]
     else:
-        randomnumber = str(random.randint(1000,9999))
+        randomnumber = str(random.randint(0,9))
         result += randomnumber
 
     k += 1

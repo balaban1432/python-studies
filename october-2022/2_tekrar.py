74c814bdbf82a643e85243070f6d46c859b32b6f
ramazan <balaban1432@gmail.com>
2023-11-23 23:03:04 +0300

    23.11.23

---
+++ october-2022/2_tekrar.py
@@ -0,0 +1,50 @@
+# e_mail = 'balaban1432@gmail.com'
+# print(e_mail)
+
+# example2 = "71"
+# print(type(example2))
+
+# example3 = 71
+# print(type(example3))
+
+# example4 = 71.0
+# print(type(example4))
+
+# example5 = 3.14j
+# print(type(example5))
+
+# example6 = True
+# print(type(example6))
+
+# f = 3.14
+# s = str(f)
+# print(type(s))
+# print(s)
+
+# f = 3.14
+# i = int(f)
+# print(i)
+# print(type(i))
+
+# i = 3
+# f = float(i)
+# print(f)
+# print(type(f))
+
+# x = 39
+# v = "11"
+# y = "2.5"
+# z = "I am at_"
+
+# print(x - int(v))
+# print(x - float(y))
+# print(type(x - float(y)))
+# print(z + str(x))
+
+# a = 36.5
+# b = '30'
+# c = '3.5'
+# d = 'F is enough for room temperature'
+
+# print(str(a + int(b) + float(c)) + d)
+

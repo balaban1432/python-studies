7c1a524a8ca423e2aa2b660b5c82fa479e6720c4
ramazan <balaban1432@gmail.com>
2023-12-01 13:31:13 +0300

    1.12.23

--- october-2022/3_tekrar.py
+++ october-2022/3_tekrar.py
@@ -19,6 +19,20 @@
 
 # print('Result of this (12+7) sum :', 12 + 7)
 
-print('C:\\november\number_expenditure.txt')
-print('C:\\november\\number_expenditure.txt')
-print('C:\november\number_expenditure.txt')
+# print('C:\\november\number_expenditure.txt')
+# print('C:\\november\\number_expenditure.txt')
+# print('C:\november\number_expenditure.txt')
+
+
+# print("one", "two", "three")
+# print("one", "two", "three", sep="\t")
+# print("one", "two", "three", sep="\n")
+# print("one", "two", "three", sep="\b")  # separates expressions by space, bir geriyi siliyor, harf varsa harfi boşluk varsa boşluğu
+
+
+# print('we', 'are', 'united') 
+# print('we', '\bare', '\bunited') 
+
+
+# print("it's funny to learn python")
+# print('it\'s funny to learn python')
